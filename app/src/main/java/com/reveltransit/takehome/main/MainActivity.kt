24ac60a29238852676ef583e1a0830114a5514a6
amohnacs15 <amohnacs@dramafever.com>
2020-12-09 13:33:52 -0500

    Map marker layer updates with api completion

--- app/src/main/java/com/reveltransit/takehome/main/MainActivity.kt
+++ app/src/main/java/com/reveltransit/takehome/main/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.reveltransit.takehome.main
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.mapbox.geojson.Feature
+import com.mapbox.geojson.FeatureCollection
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.annotations.MarkerOptions
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory.*
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 import com.reveltransit.takehome.R
 import com.reveltransit.takehome.RevelApp
 import com.reveltransit.takehome.databinding.ActivityMainBinding
@@ -19,8 +24,9 @@ import javax.inject.Inject
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     private val mapView by lazy { binding.mapView }
+
+    private lateinit var binding: ActivityMainBinding
     private lateinit var symbolManager: SymbolManager
 
     @Inject lateinit var viewModel: MainViewModel
@@ -40,22 +46,25 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.getVehicles()
         viewModel.vehicleMutableLiveData.observe(this, Observer {
-            addMarkersToMap(mapView, it)
+            addMarkersToMap(it)
         })
     }
 
-    private fun addMarkersToMap(
-        mapView: MapView,
-        vehicles: List<Vehicle>
-    ) {
-        val markerList: ArrayList<MarkerOptions> = ArrayList()
+    private fun addMarkersToMap(vehicles: List<Vehicle>) {
+        val symbolLayerIconFeatureList: ArrayList<Feature> = ArrayList()
         vehicles.forEach { vehicle ->
             vehicle.sensors?.let {
-                    markerList.add(MarkerOptions().position(LatLng(it.lat, it.lng)))
+                symbolLayerIconFeatureList.add(
+                    Feature.fromGeometry(Point.fromLngLat(it.lng, it.lat))
+                )
             }
         }
         mapView.getMapAsync { map ->
-            map.addMarkers(markerList)
+            map.getStyle { style ->
+                val featureCollection = FeatureCollection.fromFeatures(symbolLayerIconFeatureList)
+                val source = style.getSourceAs<GeoJsonSource>(SOURCE_ID)
+                source?.setGeoJson(featureCollection)
+            }
         }
     }
 
@@ -64,8 +73,25 @@ class MainActivity : AppCompatActivity() {
             map.setStyle(Style.MAPBOX_STREETS) { style ->
                 symbolManager = SymbolManager(mapView, map, style)
                 setupAnnotations(symbolManager)
+                initMarkerSymbolLayer(style)
             }
         }
+    }
+
+    private fun initMarkerSymbolLayer(style: Style) {
+        style.addImage(
+            ICON_ID,
+            BitmapFactory.decodeResource(resources, R.drawable.mapbox_marker_icon_default)
+        )
+        style.addSource(GeoJsonSource(SOURCE_ID))
+        style.addLayer(
+            SymbolLayer(LAYER_ID, SOURCE_ID)
+                .withProperties(
+                    iconImage(ICON_ID),
+                    iconAllowOverlap(true),
+                    iconIgnorePlacement(true)
+                )
+        )
     }
 
     private fun setupAnnotations(manager: SymbolManager) {
@@ -73,7 +99,6 @@ class MainActivity : AppCompatActivity() {
                 .withLatLng(LatLng(40.6677759, -74.0122655))
                 .withIconImage("rocket-15")
                 .withIconSize(1.5f)
-
         manager.create(options)
     }
 
@@ -113,6 +138,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
+        private const val ICON_ID = "DEFAULT_ICON_ID"
         private const val SOURCE_ID = "GEO_SOURCE_ID"
         private const val LAYER_ID = "GEO_LAYER_ID"
     }
